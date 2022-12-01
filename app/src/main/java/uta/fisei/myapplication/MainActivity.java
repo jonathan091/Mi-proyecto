873d24421904bdb308194a66bda6b4c208a67b30
jonathan <jonathanmatza09@gmail.com>
2022-12-01 16:31:50 -0500

    segundo github 3:29

--- app/src/main/java/uta/fisei/myapplication/MainActivity.java
+++ app/src/main/java/uta/fisei/myapplication/MainActivity.java
@@ -3,12 +3,15 @@ package uta.fisei.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
